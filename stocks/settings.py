3c4f3fa139f560d2bb1025362b7e3291eda3ecf8
tariqsupert <tariqsupert@gmail.com>
2021-04-16 19:16:38 +0100

    Update of requirments and settings files

--- stocks/settings.py
+++ stocks/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import environ
+import django-heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,6 +130,8 @@ STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = "home"
 LOGIN_URL = "login"
+
+django-heroku.settings(locals())
 
 
 
